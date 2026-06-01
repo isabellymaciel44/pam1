75833478bacb89d5fb0aa0486a07c11373ab36df
isabellymaciel44 <isabellymaciel277@gmail.com>
2026-06-01 12:31:15 +0000

    abrir_vazio

---
+++ REACT_vazio/vazio/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <scrollview>
+    <View style={styles.container}>
+      <Text>teste de tela</Text>
+     </View>
+      <View style={styles.cont_texto}>
+       <text> ...
+        </text> 
+      </View>
+     </scrollview>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
